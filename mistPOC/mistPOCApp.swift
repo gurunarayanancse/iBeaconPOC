f06621e400a6f591714c95cf622032e608bc237f
Guru Narayanan <gurunarayanancse@gmail.com>
2023-03-06 18:48:50 +0530

    Initial Commit

---
+++ mistPOC/mistPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  mistPOCApp.swift
+//  mistPOC
+//
+//  Created by Guru Narayanan on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct mistPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
